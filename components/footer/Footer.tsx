fdf0bb9cc7ba2daca85102722a90a96085a370dd
LeO <devgru0114@gmail.com>
2023-04-24 09:38:19 -0400

    add footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Container from '@/components/common/Container'
 
 const Footer = () => (
-  <footer className="py-10 md:py-20">
+  <footer className="pt-10 md:pt-20">
     <Container>
       <h3 className="mb-16 text-center text-2xl font-semibold">
         Trusted by the world&lsquo;s most innovative teams
@@ -44,7 +44,7 @@ const Footer = () => (
         </div>
       </div>
     </Container>
-    <Container>
+    <Container className='pb-20'>
       <div className="flex flex-col gap-20 md:flex-row">
         <div className="basis-2/5 text-center md:text-left">
           <h2 className="mb-7 text-2xl font-bold">
@@ -98,6 +98,60 @@ const Footer = () => (
         </div>
       </div>
     </Container>
+    <div className="bg-gray-50">
+      <Container>
+        <h2 id="footer-heading" className="sr-only">
+          Footer
+        </h2>
+        <div className="py-20">
+          <div className="md:grid md:grid-cols-4 md:gap-8">
+            <div className="space-y-8 xl:col-span-1">
+              <Image
+                src="/logo-light-with-purple.png"
+                alt="Addlandia Labs"
+                width={50}
+                height={35}
+              />
+              <p className="text-base">
+                <span className="block">Empowering everyone to thrive</span>
+                <span className="block">and prosper in Web3.</span>
+              </p>
+            </div>
+            <div className="mt-12 grid grid-cols-1 gap-8 md:col-span-2 md:col-start-3 md:grid-cols-2 xl:mt-0">
+              <div>
+                <h3 className="mb-4 text-sm font-medium uppercase tracking-wider text-gray-400">
+                  Contact
+                </h3>
+
+                <a
+                  href="mailto:contact@addlandia.com"
+                  className="text-base text-gray-500 hover:text-gray-800"
+                >
+                  contact@addlandia.com
+                </a>
+              </div>
+
+              <div>
+                <h3 className="mb-4 text-sm font-medium uppercase tracking-wider text-gray-400">
+                  Address
+                </h3>
+
+                <p className="text-gray-500">United House</p>
+                <p className="text-gray-500">9 Pembridge Road</p>
+                <p className="text-gray-500">London</p>
+                <p className="text-gray-500">W11 3JY</p>
+                <p className="text-gray-500">United Kingdom</p>
+              </div>
+            </div>
+          </div>
+          <div className="mt-12 border-t border-gray-700 pt-8">
+            <p className="text-center text-base text-gray-600">
+              &copy; 2023 Addlandia Ltd. All Rights Reserved.
+            </p>
+          </div>
+        </div>
+      </Container>
+    </div>
   </footer>
 )
 
--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -6,7 +6,7 @@ import type { LayoutProps } from './Layout.def'
 
 const Layout: FC<LayoutProps> = ({ children, hasAnimatedLogo }) => (
   <div className="relative">
-    <div className="absolute h-full w-full overflow-hidden">
+    <div className="absolute h-full w-full overflow-hidden pointer-events-none">
       <div className="absolute -left-96 -top-96 h-[600px] w-[600px] rounded-full bg-gradient-radial from-purple-300 via-transparent to-transparent blur-[150px]"></div>
       <div className="absolute -left-10 top-0 h-[600px] w-[600px] rounded-full bg-gradient-radial from-purple-300 via-transparent to-transparent blur-[160px]"></div>
       <div className="absolute left-[550px] -top-32 h-[600px] w-[600px] rounded-full bg-gradient-radial from-primary via-transparent to-transparent blur-[180px]"></div>
