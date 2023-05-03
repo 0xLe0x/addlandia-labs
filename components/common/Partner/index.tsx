25748ab8cd6fa46722b1aa647c94b9a9c5b276c4
LeO <devgru0114@gmail.com>
2023-05-03 15:02:07 -0400

    replace partners

--- components/common/Partner/index.tsx
+++ components/common/Partner/index.tsx
@@ -1,25 +1,44 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 import Container from '../Container'
 import Typography from '../Typography'
+
+const partners = [
+  {
+    image: '/images/heatstroke.svg',
+    link: '#',
+  },
+  {
+    image: '/images/kryptoria.svg',
+    link: 'https://kryptoria.io',
+  },
+  {
+    image: '/images/addland.svg',
+    link: 'https://addland.com',
+  },
+  {
+    image: '/images/nutmeg.svg',
+    link: 'https://www.nutmeg.com/',
+  },
+]
 
 const PartnerSection = () => (
   <Container className="py-28 lg:py-56">
     <Typography variant="h4" className="mb-16 text-center">
       Supporting innovative teams every step of the way
     </Typography>
-    <div className="flex flex-wrap justify-center gap-y-4">
-      {Array(2)
-        .fill(0)
-        .map((_val, i) => (
+    <div className="flex flex-col justify-around gap-y-10 sm:flex-row">
+      {partners.map((partner, i) => (
+        <Link href={partner.link} key={`partner_${i + 1}`}>
           <Image
-            src={`/images/partner_${i + 1}.svg`}
+            src={partner.image}
             fill
             className="!static mx-auto !h-[70px] !w-auto"
             alt={`partner_${i + 1}`}
-            key={`partner_${i + 1}`}
           />
-        ))}
+        </Link>
+      ))}
     </div>
   </Container>
 )
