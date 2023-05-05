af2c369109bdc0f2111f9dc1078693b9b1167817
LeO <devgru0114@gmail.com>
2023-05-05 09:51:51 -0400

    add blog main image

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -42,6 +42,7 @@ const ptComponents = {
 const Post = ({ post }) => {
   const {
     title = 'Missing title',
+    mainImage,
     name = 'Missing name',
     publishedAt,
     categories,
@@ -49,6 +50,10 @@ const Post = ({ post }) => {
     body,
   } = post ?? {
     title: 'Missing title',
+    mainImage: {
+      _type: 'image',
+      asset: [],
+    },
     name: 'Missing name',
     publishedAt: '',
     categories: null,
@@ -61,38 +66,49 @@ const Post = ({ post }) => {
   return (
     <>
       {post && (
-        <div className="flex justify-center">
-          <div className="w-full max-w-5xl px-6 py-24 lg:px-9 lg:py-40">
-            <div className="mb-10">
-              <div className="mb-8 flex gap-5">
-                {authorImage && (
-                  <div>
-                    <Image
-                      src={urlFor(authorImage).width(50).url()}
-                      width={50}
-                      height={50}
-                      alt={name}
-                      className="rounded-full"
-                    />
-                  </div>
-                )}
-                <div className="flex flex-col justify-center gap-1">
-                  <Typography variant="subtitle4">{name}</Typography>
-                  <Typography variant="subtitle4">
-                    {new Date(publishedAt).toDateString()}
-                  </Typography>
-                </div>
-              </div>
-              <Typography variant="h4">{title}</Typography>
-              {categories && (
-                <ul>
-                  {categories.map((category) => (
-                    <li key={category}>{category}</li>
-                  ))}
-                </ul>
-              )}
+        <div className="flex justify-center py-24 lg:py-40">
+          <div>
+            <div className="w-full max-w-5xl sm:px-6 lg:px-9">
+              <Image
+                src={urlFor(mainImage).width(1600).url()}
+                width={1600}
+                height={900}
+                alt={name}
+                className="mb-10 w-full"
+              />
             </div>
-            <PortableText value={body} components={ptComponents} />
+            <div className="w-full max-w-5xl px-6 lg:px-9">
+              <div className="mb-10">
+                <div className="mb-8 flex gap-5">
+                  {authorImage && (
+                    <div>
+                      <Image
+                        src={urlFor(authorImage).width(50).url()}
+                        width={50}
+                        height={50}
+                        alt={name}
+                        className="rounded-full"
+                      />
+                    </div>
+                  )}
+                  <div className="flex flex-col justify-center gap-1">
+                    <Typography variant="subtitle4">{name}</Typography>
+                    <Typography variant="subtitle4">
+                      {new Date(publishedAt).toDateString()}
+                    </Typography>
+                  </div>
+                </div>
+                <Typography variant="h4">{title}</Typography>
+                {categories && (
+                  <ul>
+                    {categories.map((category) => (
+                      <li key={category}>{category}</li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+              <PortableText value={body} components={ptComponents} />
+            </div>
           </div>
         </div>
       )}
@@ -103,6 +119,7 @@ const Post = ({ post }) => {
 
 const query = groq`*[_type == "post" && slug.current == $slug][0]{
   title,
+  mainImage,
   "name": author->name,
   publishedAt,
   "categories": categories[]->title,
@@ -124,6 +141,7 @@ export async function getStaticPaths() {
 export async function getStaticProps(context) {
   const { slug = '' } = context.params
   const post = await client.fetch(query, { slug })
+  console.log(post)
   return {
     props: {
       post,
